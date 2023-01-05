4a6614b73aed1eb1a70681330d2c3dc6cdfa5082
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-01-05 17:08:13 +0100

    Animate NavBar component with framer motion

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import './NavBar.scss';
+import React from "react";
+import "./NavBar.scss";
+import { motion } from "framer-motion";
 
-export default function NavBar () {
+export default function NavBar() {
   return (
-    <nav>
-        <img src={require("../../assets/images/icon.jpg")}  />
-        <h3 className="first">Über mich</h3>
-        <h3>Portfolio</h3>
-        <h3>Skills</h3>
-        <h3>Lebenslauf</h3>
-        <button className="btn-nav" type="button">Kontakt</button>
-    </nav>
+    <motion.nav
+      initial={{ opacity: 0, scale: 0.25 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ type: "spring", duration: 0.7 }}
+    >
+      <img src={require("../../assets/images/icon.jpg")} />
+      <h3 className="first">Über mich</h3>
+      <h3>Portfolio</h3>
+      <h3>Skills</h3>
+      <h3>Lebenslauf</h3>
+      <button className="btn-nav" type="button">
+        Kontakt
+      </button>
+    </motion.nav>
   );
 }
-
