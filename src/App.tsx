9200e21de5edf32d748305fc1bc1d1198920352f
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-01-05 16:32:50 +0100

    Add hero section elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,17 @@ import './Hero.scss';
 export default function Hero () {
   return (
     <div className="container-hero">
-      
+        <div className="container-avatar">
+            <img className="img-avatar" src={require("../../assets/images/hero.png")} />
+        </div>
+
+        <h2 className="name">Gianluca Jahn</h2>
+        <h1 className="title">Frontend-Entwickler</h1>
+        <h4>Freund des kreativen Prozesses, lebenslangen Lernens und des ständigen Wandels der Frontend-Welt.</h4>
+        <button>
+            <h3>Mehr erfahren</h3>
+            <img src={require("../../assets/images/arrow.png")} />
+        </button>
     </div>
   );
 }
