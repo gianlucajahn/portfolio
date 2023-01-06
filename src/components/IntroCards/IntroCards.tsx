031b26156c6c502873c5c54ce6e6f4211d72be0c
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-01-06 18:32:05 +0100

    Add scroll-triggered animation to small-card components using useRef on an ancestor element w/ framer motion

--- src/components/IntroCards/IntroCards.tsx
+++ src/components/IntroCards/IntroCards.tsx
@@ -1,48 +1,70 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { store } from '../../App';
+import { motion } from "framer-motion";
 import './IntroCards.scss';
 
 export default function IntroCards () {
   const [state, dispatch] = useContext(store);
+  const scrollRef = useRef(null);
 
   return (
     <div className="container-intro">
+      <div className="scroll-trigger" />
       <section className="left-intro big-card">
         <h3>{state.language === "english" ? "React-Entwickler an der Schnittstelle von Design und Code." : "React developer at the intersection between design and code"}</h3>
         <p>{state.language === "english" ? "Angefangen mit HTML, CSS und JavaScript habe ich mir eigenständig das Programmieren beigebracht. Auch jenseits von JavaScript gibt es vieles zu entdecken: Heute arbeite ich mit TypeScript in React, verwende Redux oder Context zum State-Management und nutze SASS als Pre-Processor für CSS. Wie man sieht arbeite ich desweiteren gern mit Motion Libraries - bspw. Framer Motion." : "Starting with HTML, CSS and JavaScript, I taught myself programming on my own. However, nowadays there is a lot to discover far beyond vanilla JavaScript: Today I work with TypeScript in React, use Redux or Context for state management and use SASS as a pre-processor for CSS. As you can see, I also like to work with motion libraries - e.g. Framer Motion."}</p>
       </section>
 
-      <section className="right-intro">
-        <div className="small-card">
+      <section className="right-intro" ref={scrollRef}>
+        <motion.div className="small-card"
+          initial={{ scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true, root: scrollRef }}
+        >
             <div className="card-top">
                 <h2 className="num green">200</h2>
                 <h2 className="sign green-sign">+</h2>
             </div>
             <h4>GitHub Stars</h4>
-        </div>
+        </motion.div>
 
-        <div className="small-card">
+        <motion.div className="small-card"
+            initial={{ scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.3 }}
+            viewport={{ once: true, root: scrollRef }}
+        >
             <div className="card-top">
                 <h2 className="num place blue">{state.language === "english" ? "1. Platz" : "1st place"}</h2>
             </div>
             <h4>{state.language === "english" ? `meistgeliketes Projekt auf "The Odin Project"` : `most liked project of "The Odin Project"`}</h4>
-        </div>
+        </motion.div>
 
-        <div className="small-card">
+        <motion.div className="small-card"
+            initial={{ scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.6 }}
+            viewport={{ once: true, root: scrollRef }}
+        >
             <div className="card-top">
                 <h2 className="num purple">15</h2>
                 <h2 className="sign purple-sign">+</h2>
             </div>
             <h4>{state.language === "english" ? "Projekte" : "Projects"}</h4>  
-        </div>
+        </motion.div>
 
-        <div className="small-card">
+        <motion.div className="small-card"
+            initial={{ scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.9 }}
+            viewport={{ once: true, root: scrollRef }}
+        >
             <div className="card-top">
                 <h2 className="num red">100</h2>
                 <h2 className="sign percent red-sign">%</h2>
             </div>
             <h4>{state.language === "english" ? "Leidenschaft" : "passion"}</h4>
-        </div>
+        </motion.div>
       </section>
     </div>
   );
