d05811754da285da6a965be9f72229b73ad59e69
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-01-06 14:37:55 +0100

    Add english translation to Manifest component

--- src/components/Manifest/Manifest.tsx
+++ src/components/Manifest/Manifest.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Manifest.scss';
 import { motion } from "framer-motion";
+import { store } from '../../App';
 
 export default function Manifest () {
+  const [state, dispatch] = useContext(store);
+
   return (
     <div className="container-manifest">
         <motion.a className="manifest"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
         >
-            <h1>Ambitionierter Entwickler mit Vorliebe für beeindruckende User-Experiences</h1>
+            <h1>{state.language === "english" ? "Ambitionierter Entwickler mit Vorliebe für beeindruckende User-Experiences" : "Ambitious developer with a passion to build impressive user experiences"}</h1>
         </motion.a>
     </div>
   );
