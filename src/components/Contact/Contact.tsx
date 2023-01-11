a86081078c3db49aff5077a33f944ff5d6ca6644
Gianluca Jahn <gianluca.jahn98@gmail.com>
2023-01-11 20:36:12 +0100

    Add social buttons and notifications for contact data component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,38 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { store } from '../../App';
+import { ReactComponent as LinkedIn } from "../../assets/images/linkedin.svg";
+import { ReactComponent as GitHub } from "../../assets/images/github-mark.svg";
+import { ReactComponent as Twitter } from "../../assets/images/twitter.svg";
+import { ReactComponent as WhatsApp } from "../../assets/images/whatsapp.svg";
+import toast, { Toaster } from "react-hot-toast";
 import ContactForm from '../ContactForm/ContactForm';
 import './Contact.scss';
 
 export default function Contact () {
+  const [state, dispatch] = useContext(store);
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText("gianluca.jahn98@gmail.com");
+    toast(state.language === "english" ? "E-Mail kopiert!" : "Copied E-Mail!", {
+      icon: "📧",
+      position: "top-left",
+      style: {
+        border: "1px solid #ebebeb",
+        boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.225)",
+        borderRadius: "28px",
+        fontFamily: "Inter",
+        color: "#1d1d1f",
+        fontSize: "14px",
+      },
+    });
+  };
+
   return (
     <div className="contact-container">
-        <h3 className="eyebrow">Kontakt</h3>
-        <h1 className="heading">Meine <br/>Kontakt-Daten.</h1>
+        <h3 className="eyebrow">{state.language === "english" ? "Kontakt" : "Contact"}</h3>
+        <h1 className="heading">{state.language === "english" ? "Meine Kontakt-Daten." : "My contact information."}</h1>
 
-        <p>Füllen Sie das Formular auf der linken Seite mit Ihren Informationen aus und ich werde mich so schnell wie möglich bei Ihnen melden.
-        Alternativ können Sie mich auch auf den rechts aufgeführten Plattformen finden.</p>
+        <p>{state.language === "english" ? "Füllen Sie das Formular auf der linken Seite mit Ihren Informationen aus und ich werde mich so schnell wie möglich bei Ihnen melden. Alternativ können Sie mich auch auf den rechts aufgeführten Plattformen finden." : "Fill in the form on the left with your information and I will get back to you as soon as possible. Alternatively, you can find me on the platforms listed on the right."}</p>
 
         <div className="contact-data">
             <ContactForm />
 
             <section className="contact-socials-container">
-                <button className="contact-socials-item">
+                <button className="contact-socials-item linkedin">
+                    <LinkedIn className="svg" />
                     LinkedIn
                 </button>
 
-                <button className="contact-socials-item">
+                <button className="contact-socials-item github">
+                    <GitHub className="svg" />
                     GitHub
                 </button>
 
-                <button className="contact-socials-item">
+                <button className="contact-socials-item twitter">
+                    <Twitter className="svg" />
                     Twitter
                 </button>
 
-                <button className="contact-socials-item">
+                <button className="contact-socials-item whatsapp">
+                    <WhatsApp className="svg" />
                     WhatsApp
                 </button>
 
-                <button className="contact-socials-email">
-                    E-Mail-Adresse kopieren
+                <button className="email" onClick={copyEmail}>
+                    {state.language === "english" ? "E-Mail kopieren" : "Copy my email"}
                 </button>
             </section>
         </div>
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -2,13 +2,27 @@ import React, { useContext, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "./ContactForm.scss";
 import { store } from "../../App";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ContactForm() {
   const form = useRef<any>(null);
   const [state, dispatch] = useContext(store);
+  const input1 = useRef(null), input2 = useRef(null), input3 = useRef(null);
 
   const sendEmail = (e: any) => {
     e.preventDefault();
+
+    toast.success(state.language === "english" ? "Nachricht abgeschickt!" : "Message sent!", {
+        position: "top-left",
+        style: {
+        border: "1px solid #ebebeb",
+        boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.225)",
+        borderRadius: "28px",
+        fontFamily: "Inter",
+        color: "#1d1d1f",
+        fontSize: "14px",
+        },
+    });
 
     emailjs
       .sendForm(
@@ -25,28 +39,41 @@ export default function ContactForm() {
           console.log(error.text);
         }
       );
+
+    // clear fields after submitting message
+    let fields = document.getElementsByClassName('field');
+    let field1 = fields[0] as HTMLInputElement, field2 = fields[1] as HTMLInputElement, field3 = fields[2] as HTMLTextAreaElement;
+    field1.value = "";
+    field2.value = "";
+    field3.value = "";
   };
 
   return (
     <form ref={form} onSubmit={sendEmail} className="contact-form-container">
       <div className="input-container">
         <input
+          ref={input1}
           className="field"
           type="text"
           name="user_name"
           placeholder={state.language === "english" ? "Ihr Name" : "Your name"}
+          required
         />
         <input
+          ref={input2}
           className="field"
           type="email"
           name="user_email"
           placeholder={state.language === "english" ? "Ihre E-Mail-Adresse" : "Your email address"}
+          required
         />
       </div>
       <textarea
+        ref={input3}
         name="message"
         placeholder={state.language === "english" ? "Ihre Nachricht an mich" : "Your message for me"}
         className="field"
+        required
       />
       <input type="submit" value={state.language === "english" ? "Absenden" : "Submit"} className="submit" />
     </form>
